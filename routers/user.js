c9c5b5a268f753d62bda08fc3a9a04b0f097415b
Nikolay Dolgov <koly201020@yandex.ru>
2022-07-07 10:53:27 +0300

    Fix errors

--- routers/user.js
+++ routers/user.js
@@ -7,7 +7,7 @@ const {
 // const regex = /[https?:\\/\\/][w{3}.]?[a-zA-Z0-9\\\-._~:/?#\\[\\]@!\$&'()*+,;=]+[#]?/;
 // const regex = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\\/]))?/;
 // eslint-disable-next-line no-useless-escape
-const regex = /http[s]?:\/\/[w{3}.]?[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]+#?/;
+const regex = /http[s]?:\/\/[w{3}]?[a-zA-Z0-9-]+.[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]+#?/;
 
 router.get('/users', getUserAll);
 router.get('/users/me', getUser);
