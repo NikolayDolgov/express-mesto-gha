b77737c9621d80ed526423c394e43deece3ac7a9
Nikolay Dolgov <koly201020@yandex.ru>
2022-07-07 09:41:58 +0300

    Fix errors

--- routers/user.js
+++ routers/user.js
@@ -6,7 +6,7 @@ const {
 
 // const regex = /[https?:\\/\\/][w{3}.]?[a-zA-Z0-9\\\-._~:/?#\\[\\]@!\$&'()*+,;=]+[#]?/;
 // const regex = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\\/]))?/;
-const regex = /(http|https):\/\/[w{3}.]?[a-zA-Z0-9-]+\.^\s/;
+const regex = /(http|https):\/\/[w{3}.]?[a-zA-Z0-9-]+\.^\s{1,}/;
 
 router.get('/users', getUserAll);
 router.get('/users/me', getUser);
