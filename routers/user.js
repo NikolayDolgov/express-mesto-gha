e833b7223150277d1f8af362f0115afde2526bb9
Nikolay Dolgov <koly201020@yandex.ru>
2022-07-07 08:38:48 +0300

    Fix errors

--- routers/user.js
+++ routers/user.js
@@ -24,7 +24,7 @@ router.patch('/users/me', celebrate({
 
 router.patch('/users/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().pattern(regex).required(),
+    avatar: Joi.string().uri().required(),
   }),
 }), patchUserAvatar);
 
